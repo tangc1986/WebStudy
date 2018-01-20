c0dfe2b09b996f62e6feb150aab3b845218218c3
tangc1986 <tangc1986@gmail.com>
2018-01-21 00:19:08 +0800

    2018.01.21

--- JavaScriptDom/chapter04/scripts/showPic.js
+++ JavaScriptDom/chapter04/scripts/showPic.js
@@ -15,4 +15,21 @@ function countBodyChildren() {
     // alert(body_element.nodeType);
 }
 
-window.onload = countBodyChildren;
+window.onload = prepareLinks;
+
+function prepareLinks() {
+    if (!document.getElementsByTagName) return false;
+    var links = document.getElementsByTagName("a");
+    for (var i=0; i<links.length; i++) {
+        if (links[i].getAttribute("class") == "popup") {
+            links[i].onclick = function() {
+                popUp(this.getAttribute("href"));
+                return false;
+            }
+        }
+    }
+}
+
+function popUp(winURL) {
+    window.open(winURL, "popup", "width=320, height=480");
+}
