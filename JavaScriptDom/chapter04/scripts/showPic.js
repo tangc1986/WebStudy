7703840f6a5e11cb1411254d49b4a58d0e70da3f
tangc1986 <tangc1986@gmail.com>
2018-01-28 13:13:26 +0800

    2018.01.28

--- JavaScriptDom/chapter04/scripts/showPic.js
+++ JavaScriptDom/chapter04/scripts/showPic.js
@@ -1,10 +1,17 @@
 function showPic(whichpic) {
+    if (!document.getElementById("placeholder")) return false;
     var source = whichpic.getAttribute("href");
     var placeholder = document.getElementById("placeholder");
+    if (placeholder.nodeName != "IMG") return false;
     placeholder.setAttribute("src", source);
-    var text = whichpic.getAttribute("title");
-    var description = document.getElementById("description");
-    description.firstChild.nodeValue = text;
+    if (document.getElementById("description")) {
+        var text = whichpic.getAttribute("title") ? whichpic.getAttribute("title") : "";
+        var description = document.getElementById("description");
+        if (description.firstChild.nodeType == 3) {
+            description.firstChild.nodeValue = text;
+        }
+    }
+    return true;
 }
 
 function countBodyChildren() {
@@ -15,7 +22,7 @@ function countBodyChildren() {
     // alert(body_element.nodeType);
 }
 
-window.onload = prepareLinks;
+//window.onload = prepareLinks;
 
 function prepareLinks() {
     if (!document.getElementsByTagName) return false;
@@ -40,4 +47,23 @@ function prepareGallery() {
     if (!document.getElementById("imagegallery")) return false;
     var gallery = document.getElementById("imagegallery");
     var links = gallery.getElementsByTagName('a');
+    for (var i=0; i < links.length; i++) {
+        links[i].onclick = function() {
+            return !showPic(this);
+        }
+    }
 }
+
+function addLoadEvent(func) {
+    var oldonload = window.onload;
+    if (typeof window.onload != 'function') {
+        window.onload = func;
+    } else {
+        window.onload = function() {
+            oldonload();
+            func();
+        }
+    }
+}
+
+addLoadEvent(prepareGallery);
